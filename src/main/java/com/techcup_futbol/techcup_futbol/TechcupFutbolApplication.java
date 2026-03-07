32781a75ccbae571c9aeb06118220aa88929cde6
wombitoCol <julio.mayorquin@mail.escuelaing.edu.co>
2026-03-06 21:00:38 -0500

    maven implementation

---
+++ src/main/java/com/techcup_futbol/techcup_futbol/TechcupFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.techcup_futbol.techcup_futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcupFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcupFutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcup_futbol/techcup_futbol/TechcupFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcup_futbol.techcup_futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcupFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
